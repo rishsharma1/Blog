53371654724d243950343dcfac26631f1ed8764c
Rishabh Sharma <rishsharma13@gmail.com>
2016-02-17 03:42:15 +1100

    adding post functionality

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-
+# For rendering markdown files for the blog 
+gem 'redcarpet', '~> 3.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
 # Use sqlite3 as the database for Active Record
@@ -42,4 +43,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.delete_all
+
+Post.create(
+  id: 1,
+  title: "My Very First Post",
+  created_at: Time.now - 1.day,
+  content:
+  %Q{### There Is Something You Should Know!
+
+  This is my very first post using markdown!
+
+  How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!}
+)
+
+Post.create(
+  id: 2,
+  title: "My Second Post",
+  created_at: Time.now,
+  content:
+  %Q{### My List of Things To Do!
+
+  Here is the list of things I wish to do!
+
+  * write more posts
+  * write even more posts
+  * write even more posts!}
+)
