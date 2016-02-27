8b99ceb38caf79d8ba2a981c8496ad89c29f0bd5
Rishabh Sharma <rishsharma13@gmail.com>
2016-02-28 00:22:35 +1100

    fixing test files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
 
+
+
+  get 'users/new'
+
   resources :posts, only: [:index, :show]
   root 'posts#index'
 
   get 'posts/show'
   get 'login' => 'sessions#new'
   delete 'logout' => 'sessions#destroy'
+  get 'contact' => 'static_pages#contact'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
 
-  
+  test "layout links" do
+    get root_path
+    assert_template 'posts/index'
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+
+  end
 
 end
