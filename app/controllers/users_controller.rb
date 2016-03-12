13bfe4f881f8e032e5db8778129283e77a010bb4
Rishabh Sharma <rishsharma13@gmail.com>
2016-03-12 16:47:10 +1100

    remove redundant function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,5 @@ class UsersController < ApplicationController
   end
 
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
 end
