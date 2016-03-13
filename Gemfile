d1e8b438bd28b9bbb226a6e84d58be69bafe858c
Rishabh Sharma <rishsharma13@gmail.com>
2016-03-13 20:03:43 +1100

    adding syntax highlighting for code in md with coderayify

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'bcrypt',                  '3.1.7'
+gem 'coderay'
 
 # For the css framework
 gem 'bootstrap-sass', '3.2.0.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,30 @@
 module ApplicationHelper
 
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code,language)
+      CodeRay.scan(code,language).div
+    end
+  end
+
   def markdown(text)
-    options = {
+
+    coderayified = CodeRayify.new(:filter_html => true,
+    :hard_wrap => true)
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true,
       filter_html:     false,
       hard_wrap:       true,
       link_attributes: { rel: 'nofollow', target: "_blank" },
       space_after_headers: true,
       fenced_code_blocks: true,
-      prettify: true 
+      prettify: true
     }
 
-    extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    renderer = Redcarpet::Render::HTML.new(extensions)
+    markdown = Redcarpet::Markdown.new(coderayified, extensions)
 
     markdown.render(text).html_safe
   end
